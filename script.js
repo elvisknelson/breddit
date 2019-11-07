844d45ab5e42f3e32974edb4471f48880d867c0f
elvisknelson <elvisknelson@gmail.com>
2019-11-07 00:44:46 -0600

    Fixing User Validation

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ function hasSpaces(s)
     return s.indexOf(' ') >= 0;
 }
 
+function trim(str) 
+{
+	// Uses a regex to remove spaces from a string.
+	return str.replace(/^\s+|\s+$/g,"");
+}
+
+function hasText(s)
+{
+	if(s.value == null || trim(s.value) == "")
+	{
+		return false;
+	}
+
+	return true;
+}
+
 function throwError(er, e)
 {
     e.preventDefault();
@@ -14,6 +30,24 @@ function validate(e)
     var username = document.getElementById("newusername").value;
     var password1 = document.getElementById("newpassword1").value;
     var password2 = document.getElementById("newpassword2").value;
+
+    if(username.length < 8)
+    {
+        throwError("Username must be 8 characters or longer", e);
+    }
+    else if(password1.length < 8)
+    {
+        throwError("Password must be 8 characters or longer", e);
+    }
+
+    if(username == "")
+    {
+        throwError("Username cannot be blank", e);
+    }
+    else if(password1 == "" && password2 == "")
+    {
+        throwError("Password cannot be blank", e);
+    }
 
     if(hasSpaces(username))
     {
