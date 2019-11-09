8f14cf394edabb435bf5a603007f58cb90a13523
elvisknelson <elvisknelson@gmail.com>
2019-11-09 15:59:51 -0600

    More Corrections

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 </head>
 <body>
     <div class="footer">
-        <p>This site is purely for demenstration purpuses only. Please don't sue me reddit :)</p>
+        <p>This site was designed for a school project and is for demonstration purposes only. Please don't sue me reddit :)</p>
         <p>BREDDIT and the DOPE ALIEN Logo are registered trademarks of breddit inc. (not really)</p>
         <p><a href="https://www.linkedin.com/in/elvis-knelson/">About Me</a></p>
     </div>
