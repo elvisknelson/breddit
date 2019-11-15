a9b2f185ca5008c713cb16fb2b284077db713702
elvisknelson <elvisknelson@gmail.com>
2019-11-14 20:29:05 -0600

    Errors

--- newuser.php
+++ newuser.php
@@ -9,14 +9,14 @@
         $password1 = $_POST['password1'];
         $password2 = $_POST['password2'];
 
-        if($username == 'elfishpro' || $username == 'admin' || $username == 'namename')
-        {
-            $ismod = true;
-        }
-        else
-        {
-            $ismod = false;
-        }
+        // if($username == 'elfishpro' || $username == 'admin' || $username == 'namename')
+        // {
+        //     $ismod = true;
+        // }
+        // else
+        // {
+        //     $ismod = false;
+        // }
 
         $insertQuery = "INSERT INTO users (username, password, moderator) VALUES (:name, :password, :moderator)";
         $statement = $db->prepare($insertQuery);
@@ -25,12 +25,12 @@
         $statement->bindValue(':moderator', $ismod);
         $statement->execute();
 
-        $query = "SELECT id FROM users WHERE username = (:user)";
-        $values = $db->prepare($query);
-        $values->bindValue(':user', $username);
-        $values->execute();
-        $row = $values->fetch();
-        $_SESSION['user'] = [ 'name' => $username, 'id' => 81,'mod' => $ismod ];
+        // $query = "SELECT id FROM users WHERE username = (:user)";
+        // $values = $db->prepare($query);
+        // $values->bindValue(':user', $username);
+        // $values->execute();
+        // $row = $values->fetch();
+        // $_SESSION['user'] = [ 'name' => $username, 'id' => $,'mod' => true ];
 
         header('Location: index.php');
     }
@@ -45,7 +45,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="script.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
@@ -86,6 +85,6 @@
     </div>
     <?php include 'footer.php'; ?>
 
-   
+    <script src="script.js"></script>
   </body>
 </html>
