462fdab91b86aee9a39e8a7270c2fcbbffd065ae
elvisknelson <elvisknelson@gmail.com>
2020-03-11 18:35:18 -0500

    Test

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+    if(isset($_POST['test']))
+    {
+        $file = 'people.txt';
+        // The new person to add to the file
+        $person = "John Smith\n";
+        // Write the contents to the file, 
+        // using the FILE_APPEND flag to append the content to the end of the file
+        // and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+        file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +19,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Hello World</h1>
 </body>
 </html>
