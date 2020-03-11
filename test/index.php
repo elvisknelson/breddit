38f66d93cecdd67b43a5b3fe182a7b276d48b81d
elvisknelson <elvisknelson@gmail.com>
2020-03-11 18:32:37 -0500

    Testing Api

---
+++ test/index.php
@@ -0,0 +1,38 @@
+<?php
+  session_start();
+
+  if(!isset($_SESSION['sort'])) {
+    $_SESSION['sort'] = "date";
+  }
+  else {
+    if(isset($_GET['sort'])) {
+      $_SESSION['sort'] = $_GET['sort'];
+    }
+    else {
+      $_SESSION['sort'] = "date";
+    }
+  }
+?>
+
+<!doctype html>
+<html lang="en">
+  <?php include 'head.php' ?>
+  <body>
+    <div style="display: block" id="hideAll">&nbsp;</div>
+    <?php include 'header.php'; ?>
+    
+    <div id="wrapper">
+      <div id="content">
+        <?php include 'sidebar.php'; ?>
+        <div id='main'>
+        
+        </div>
+      </div>
+    </div>
+        
+    <div class="loadmore">
+      <a id="load">Load More</a>
+    </div>
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
