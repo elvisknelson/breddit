921c02378843886a2b787bee3c7ad8df62efd6a0
elvisknelson <elvisknelson@gmail.com>
2020-03-10 21:11:23 -0500

    Testing API

--- utility/process_post.php
+++ utility/process_post.php
@@ -15,7 +15,7 @@
                 $query = "INSERT INTO subbreddit (name, moderator) VALUES (:name, :mod)";
                 $statement = $db->prepare($query);
                 $statement->bindValue(':name', $subbreddit);
-                $statement->bindValue(':mod', 'Elvis The God';
+                $statement->bindValue(':mod', 'Elvis The God');
                 $statement->execute();
             }
 
