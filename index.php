5ecc2fd51de84077c23d80f8f1c5a5678812f5d1
Elvis Knelson <elfishpro@hotmail.com>
2020-04-30 21:50:24 -0500

    moving files

--- index.php
+++ index.php
@@ -1,12 +1,38 @@
-<!DOCTYPE html>
+<?php
+  session_start();
+ 
+  if(!isset($_SESSION['sort'])) {
+    $_SESSION['sort'] = "date";
+  }
+  else {
+    if(isset($_GET['sort'])) {
+      $_SESSION['sort'] = $_GET['sort'];
+    }
+    else {
+      $_SESSION['sort'] = "date";
+    }
+  }
+?>
+
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>breddit</title>
-</head>
-<body>
-    <h1>Under Construction</h1>
-    <h2>Be back soon</h2>
-</body>
+  <?php include 'head.php' ?>
+  <body>
+    <div style="display: block" id="hideAll">&nbsp;</div>
+    <?php include 'header.php'; ?>
+    
+    <div id="wrapper">
+      <div id="content">
+        <?php include 'sidebar.php'; ?>
+        <div id='main'>
+        
+        </div>
+      </div>
+    </div>
+        
+    <div class="loadmore">
+      <a id="load">Load More</a>
+    </div>
+    <?php include 'footer.php'; ?>
+  </body>
 </html>
